036ca55470c3605a363a0372a05d4478a02f89e1
Fernando Aranzabe <faranzabe@gmail.com>
2023-01-05 19:04:21 +0100

    Autorización con Firebase 2023

--- AutenticacionFirebase2023/app/src/main/java/com/example/autenticacionfirebase2023/HomeActivity.kt
+++ AutenticacionFirebase2023/app/src/main/java/com/example/autenticacionfirebase2023/HomeActivity.kt
@@ -2,10 +2,16 @@ package com.example.autenticacionfirebase2023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.autenticacionfirebase2023.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+//        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
